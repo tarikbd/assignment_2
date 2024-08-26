26f0f652f28aa787920901850a322d0c552d9ff2
Tarik <tarikzaman.bd@gmail.com>
2024-08-26 19:13:25 +0600

    Added files vie upload
    Assignment-2 - Batch 4 - Module 10

---
+++ oop.php
@@ -0,0 +1,67 @@
+<?php
+class Book {
+    private $title;
+    private $availableCopies;
+
+    public function __construct($title, $availableCopies) {
+        $this->title = $title;
+        $this->availableCopies = $availableCopies;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getAvailableCopies() {
+        return $this->availableCopies;
+    }
+
+    public function borrowBook() {
+        if ($this->availableCopies > 0) {
+            $this->availableCopies--;
+            return true;
+        }
+        return false;
+    }
+
+    public function returnBook() {
+        $this->availableCopies++;
+    }
+}
+
+class Member {
+    private $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function borrowBook(Book $book) {
+        if ($book->borrowBook()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function returnBook(Book $book) {
+        $book->returnBook();
+    }
+}
+
+// Usage
+$book1 = new Book("The Great Gatsby", 5);
+$book2 = new Book("To Kill a Mockingbird", 3);
+
+$member1 = new Member("John Doe");
+$member2 = new Member("Jane Smith");
+
+$member1->borrowBook($book1);
+$member2->borrowBook($book2);
+
+echo "Available Copies of '" . $book1->getTitle() . "': " . $book1->getAvailableCopies() . PHP_EOL;
+echo "Available Copies of '" . $book2->getTitle() . "': " . $book2->getAvailableCopies() . PHP_EOL;
+?>
